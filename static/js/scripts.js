18913c2f043768a3285149c7714242a3cbc90fa0
Brock Larson <brock.larson0725@gmail.com>
2023-05-01 22:42:30 -0600

    Add remove book functionality with API request

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,18 +1,13 @@
-const addBookModule = (() => {
+const formModule = (() => {
     //CACHE DOM
     const formBackground = $(`#formBackground`)[0];
     const formContainer = $(`#formContainer`)[0];
-    const submitButton = $(`#submitButton`)[0];
-    const addBookButton = $(`#addBookButton`)[0];
     const exitForm = $(`#closeForm`)[0];
 
-    //LISTENERS
-    addBookButton.addEventListener(`click`, showForm);
-    exitForm.addEventListener(`click`, closeForm);
-    formBackground.addEventListener(`click`, closeForm);
-    submitButton.addEventListener(`click`, submitBook);
+    // LISTENERS
+    exitForm.addEventListener(`click`, formModule.closeForm);
+    formBackground.addEventListener(`click`, formModule.closeForm);
 
-    //HANDLE FORM
     function showForm() {
         $(`#bookTitle`)[0].value = ``;
         $(`#bookAuthor`)[0].value = ``;
@@ -28,7 +23,19 @@ const addBookModule = (() => {
         formBackground.classList.remove(`show`);
     }
 
-    // Form Validation
+    return { showForm, closeForm }
+})();
+
+const addBookModule = (() => {
+    //CACHE DOM
+    const submitButton = $(`#submitButton`)[0];
+    const addBookButton = $(`#addBookButton`)[0];
+
+    //LISTENERS
+    addBookButton.addEventListener(`click`, formModule.showForm);
+    submitButton.addEventListener(`click`, submitBook);
+
+    // Add Book Form Validation
     function invalidForm() {
         let invalid = false;
         const invalidIndicator = document.querySelectorAll(`small`);
@@ -56,8 +63,8 @@ const addBookModule = (() => {
     async function submitBook() {
         if (invalidForm()) return;
         await booksPOST();
-        closeForm();
-        updateTable(createRows);
+        formModule.closeForm();
+        await displayModule.updateDisplay();
     }
 
     function getJsonObject() {
@@ -84,74 +91,63 @@ const addBookModule = (() => {
         })
     }
 
-    function updateTable(callback) {
-        $.ajax({
-            method: "GET",
-            url: "/api/books/",
-            success: function (data) {
-                callback(data)
-            }
-        });
-    }
-
-    function createRows(data) {
-        $("tbody").empty();
-        $.each(data, function (key, value) {
-            const title = value.title;
-            const cover = value.paperback ? "Paperback" : "Hardback";
-            const authors = value.authors.map(function (author) {
-                return author['name'];
-            }).join(", ");
-            const price = value.price;
-            $("tbody").append(
-                f`<tr><td>{` + title + `}</td><td>` + authors + `</td><td>` + cover + `</td><td>$` + price + `</td><td><span class="material-icons-outlined edit">edit</span></td><td><span class="material-icons-outlined edit">close</span></td></tr>`
-            )
-        })
-    }
 })();
 
-// myLibrary = []
 const tableModule = (() => {
-    // // CACHE DOM
-    // const bookTable = document.getElementById(`tableBody`);
-    // const table = document.querySelector(`table`);
-    // const searchBar = document.getElementById(`searchBar`);
+    // CACHE DOM
+    const bookTable = $(`#tableBody`)[0];
+    const table = $(`table`)[0];
+    const searchBar = $(`#searchBar`)[0];
 
-    // //LISTENERS
-    // table.addEventListener(`click`, handleTableClick);
+    //LISTENERS
+    table.addEventListener(`click`, handleTableClick);
     // searchBar.addEventListener('input', updateTable);
 
-    // //TABLE FUNCTIONALITY
-    // function handleTableClick(event) {
-    //     if (event.target.classList.contains(`remove`)) removeBook(event);
-    //     else if (event.target.classList.contains(`edit`)) viewBookInfo(event);
-    //     else if (event.target.classList.contains(`checked-out-cell`)) changeBookStatus(event);
-    //     else if (event.target.classList.contains(`table-header-text`)) handleSorting(event);
-    // }
+    //TABLE FUNCTIONALITY
+    function handleTableClick(event) {
+        console.log(event.target);
+        if (event.target.classList.contains(`remove`)) removeBook(event);
+        else if (event.target.classList.contains(`edit`)) editBookInfo(event);
+        // else if (event.target.classList.contains(`checked-out-cell`)) changeBookStatus(event);
+        // else if (event.target.classList.contains(`table-header-text`)) handleSorting(event);
+    }
 
-    // function removeBook(event) {
-    //     const targetBook = findBookIndex(event);
-    //     myLibrary[targetBook].removeBook();
-    //     updateDisplay();
-    // }
+    function findTableRow(event) {
+        let tableRow = event.target.parentNode.parentNode;
+        return tableRow;
+    }
 
-    // function viewBookInfo(event) {
-    //     const targetBook = findBookIndex(event);
-    //     myLibrary[targetBook].showBookInfo();
-    // }
+    function findBookID(tableRow) {
+        return tableRow.querySelector('.book-id').innerText;
+    }
+
+    async function removeBook(event) {
+        const tableRow = findTableRow(event);
+        const bookID = findBookID(tableRow);
+        await bookDEL(tableRow, bookID);
+        // await displayModule.updateDisplay();
+    }
+
+    function bookDEL(row, bookID) {
+        return $.ajax({
+            type: 'DELETE',
+            url: `/api/books/${bookID}`,
+            success: function () {
+                $(row).remove();
+                console.log(`Book ${bookID} was deleted successfully`);
+            }
+        })
+    }
+
+    function editBookInfo(event) {
+        const targetBook = findBookID(event);
+        formModule.showForm();
+    }
 
     // function changeBookStatus(event) {
     //     const targetBook = findBookIndex(event);
     //     myLibrary[targetBook].checkOut();
     //     updateDisplay();
-    // }
-
-    // function findBookIndex(event) {
-    //     let rowIndexNumber = parseInt(event.target.parentNode.dataset.indexNumber);
-    //     if (event.target.tagName.toLowerCase() === `span`) {
-    //         rowIndexNumber = parseInt(event.target.parentNode.parentNode.dataset.indexNumber);
-    //     }
-    //     return myLibrary.findIndex(book => book.bookID === rowIndexNumber);
     // }
 
     // function handleSorting(event) {
@@ -189,24 +185,39 @@ const tableModule = (() => {
     //     }
     // }
 
-    // function sortLibrary(sortDirection, sortParam) {
-    //     if (sortParam === `bookID`) {
-    //         myLibrary = myLibrary.sort((a, b) => (a[sortParam] > b[sortParam]) ? 1 : -1);
-    //         return;
-    //     }
 
-    //     if (sortDirection === `ascending`) {
-    //         myLibrary = myLibrary.sort(function (a, b) {
-    //             if (a[sortParam].toString().toLowerCase() > b[sortParam].toString().toLowerCase()) return 1;
-    //             else if (a[sortParam].toString().toLowerCase() === b[sortParam].toString().toLowerCase()) return 0;
-    //             else return -1;
-    //         });
-    //     } else if (sortDirection === `descending`) {
-    //         myLibrary = myLibrary.sort(function (a, b) {
-    //             if (a[sortParam].toString().toLowerCase() > b[sortParam].toString().toLowerCase()) return -1;
-    //             else if (a[sortParam].toString().toLowerCase() === b[sortParam].toString().toLowerCase()) return 0;
-    //             else return 1;
-    //         });
-    //     }
-    // }
+
+})();
+
+const displayModule = (() => {
+    function booksGET(callback) {
+        $.ajax({
+            method: "GET",
+            url: "/api/books/",
+            success: function (data) {
+                callback(data)
+            }
+        });
+    }
+
+    function createRows(data) {
+        $("tbody").empty();
+        $.each(data, function (key, value) {
+            const title = value.title;
+            const cover = value.paperback ? "Paperback" : "Hardback";
+            const authors = value.authors.map(function (author) {
+                return author['name'];
+            }).join(", ");
+            const price = value.price;
+            $("tbody").append(
+                `<tr><td class="book-id"></td><td>` + title + `</td><td>` + authors + `</td><td>` + cover + `</td><td>$` + price + `</td><td class="icons-cell"><span class="material-icons-outlined edit">edit</span></td><td class="icons-cell"><span class="material-icons-outlined edit">close</span></td></tr>`
+            )
+        })
+    }
+
+    function updateDisplay() {
+        booksGET(createRows);
+    }
+
+    return { updateDisplay }
 })();
